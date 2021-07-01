a508d494728ca0710667dc7e7f73cb62438a7c45
vaibhavgoel0230 <vaibhav.goel_cs.aiml19@gla.ac.in>
2021-07-01 19:27:28 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyC61aw4XPih8Fi7T7FzUk8pzSG28G_uW8s',
+  authDomain: 'my-chat-app-d4855.firebaseapp.com',
+  projectId: 'my-chat-app-d4855',
+  storageBucket: 'my-chat-app-d4855.appspot.com',
+  messagingSenderId: '180655239894',
+  appId: '1:180655239894:web:f0d79e9c2c6025d7464f77',
+};
+
+const app = firebase.initializeApp(config);
