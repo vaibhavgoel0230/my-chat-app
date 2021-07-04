6f5462110b6cf58892df72c189c0f0ab1fb18b2b
vaibhavgoel0230 <vaibhav.goel_cs.aiml19@gla.ac.in>
2021-07-04 21:34:55 +0530

    created icon btn control component

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -7,6 +7,7 @@ import PresenceDot from '../../PresenceDot';
 import { useCurrentRoom } from '../../../context/current-room.context';
 import { auth } from '../../../misc/firebase';
 import { useHover } from '../../../misc/custom-hooks';
+import IconBtnControl from './IconBtnControl';
 
 const MessageItem = ({ message, handleAdmin }) => {
   const { author, createdAt, text } = message;
@@ -47,6 +48,14 @@ const MessageItem = ({ message, handleAdmin }) => {
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
+        />
+        <IconBtnControl
+          {...(true ? { color: 'red' } : {})}
+          isVisible
+          iconName="heart"
+          tooltip="Like this Message"
+          onClick={() => {}}
+          badgeContent={5}
         />
       </div>
       <div>
